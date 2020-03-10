e8503a430ca2b06fac577edbf5249837ae8111f8
Christian-Anderson <canderson@stcharlessd.org>
2020-03-10 13:34:30 -0500

    modified:   highscore_trivia_library.pickle 	modified:   question_trivia_library.pickle 	modified:   reset_trivia_library.py 	modified:   trivia_game.py

--- reset_trivia_library.py
+++ reset_trivia_library.py
@@ -6,22 +6,31 @@
 
 import pickle
 
-questions = {1:['History Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
-             2:['History Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)]'], 
+
+questions = {1:['Games Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
+             2:['Games Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)]'], 
              3:['Geography Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
              4:['Geography Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
-             5:['Music Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
-             6:['Music Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
-             7:['Games Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
-             8:['Games Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],}
+             5:['History Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
+             6:['History Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
+             7:['Music Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],
+             8:['Music Question', 'Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Correct Answer(1, 2, 3, 4)'],}
 
 datafile = open("question_trivia_library.pickle", "wb")
 pickle.dump(questions, datafile)
 datafile.close()
 
 
-highscores = {1:['FPS', 'Halo3', 'Bungie', 'Microsoft', 'Xbox', '2007', '10', '2', '30.00', 'Yes', '1/15/2008', 'This game is overrated'],
-         2:['RPG', 'Undertale', 'Toby Fox', '8-4', 'PC, PS4, PS Vita, Nintendo Switch', '2015', '10', '1', '10.00', 'Yes', '2/2/2016', 'Its pretty darn good yo']}
+highscores = {1:['Name', 'Score', 'Games Highscore'],
+              2:['Name', 'Score', 'Games Highscore'], 
+              3:['Name', 'Score', 'Geography Highscore'],
+              4:['Name', 'Score', 'Geography Highscore'],
+              5:['Name', 'Score', 'History Highscore'],
+              6:['Name', 'Score', 'History Highscore'],
+              7:['Name', 'Score', 'Music Highscore'],
+              8:['Name', 'Score', 'Music Highscore'],
+              7:['Name', 'Score', 'Gauntlet Highscore'],
+              8:['Name', 'Score', 'Gauntlet Highscore']}
 
 datafile = open("highscore_trivia_library.pickle", "wb")
 pickle.dump(highscores, datafile)
--- trivia_game.py
+++ trivia_game.py
@@ -11,12 +11,186 @@ from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox
 
 #---Constants---
-TITLE_FONT = ("Times New Roman", 24)
-BUTTON_FONT = ("Arial", 15)
+TITLE_FONT = ("Times New Roman", 48)
+BUTTON_FONT = ("Arial", 24)
+LABEL_FONT = ("Arial", 18)
 
 #---Classes---
+class Screen(tk.Frame):
+    
+    current = 0
+    
+    def __init__(self):
+        tk.Frame.__init__(self)
+        
+    def switch_frame():
+        screens[Screen.current].tkraise()
+        
+        
+class MainMenu(Screen):
+    
+    def __init__(self):
+        Screen.__init__(self)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+        self.grid_rowconfigure(7, weight=1)
+        self.grid_rowconfigure(8, weight=1)
+
+        
+        self.lbl_title = tk.Label(self,text="Trivia Game", font=TITLE_FONT)
+        self.lbl_title.grid(row=0,column=0,rowspan=2,columnspan=4,sticky="news")        
+        
+        self.btn_games = tk.Button(self,text="  Games  ",font=BUTTON_FONT,command=self.go_trivia_games)
+        self.btn_games.grid(row=3,column=1,sticky="news")  
+        
+        self.btn_geography = tk.Button(self,text="Geography",font=BUTTON_FONT,command=self.go_trivia_geography)
+        self.btn_geography.grid(row=3,column=2,sticky="news")
+        
+        self.btn_history = tk.Button(self,text="History",font=BUTTON_FONT,command=self.go_trivia_history)
+        self.btn_history.grid(row=4,column=1,sticky="news")
+        
+        self.btn_music = tk.Button(self,text="Music",font=BUTTON_FONT,command=self.go_trivia_music)
+        self.btn_music.grid(row=4,column=2,sticky="news")
+        
+        self.btn_random = tk.Button(self,text="Random",font=BUTTON_FONT,command=self.go_trivia_random)
+        self.btn_random.grid(row=5,column=1,sticky="news")     
+        
+        self.btn_gauntlet = tk.Button(self,text="Gauntlet",font=BUTTON_FONT,command=self.go_trivia_gauntlet)
+        self.btn_gauntlet.grid(row=5,column=2,sticky="news")
+        
+        self.btn_highscores = tk.Button(self,text="Highscores",font=BUTTON_FONT,command=self.go_highscores)
+        self.btn_highscores.grid(row=7,column=1,columnspan=2,sticky="news")        
+        
+        
+    def go_trivia_games(self): 
+        self.going_trivia = 1
+        Screen.current=1
+        Screen.switch_frame()
+        
+    def go_trivia_geography(self): 
+        self.going_trivia = 2
+        Screen.current=1
+        Screen.switch_frame()
+        
+    def go_trivia_history(self): 
+        self.going_trivia = 3
+        Screen.current=1
+        Screen.switch_frame()
+        
+    def go_trivia_music(self): 
+        self.going_trivia = 4
+        Screen.current=1
+        Screen.switch_frame()
+        
+    def go_trivia_random(self):   
+        self.going_trivia = 5
+        Screen.current=1
+        Screen.switch_frame()
+        
+    def go_trivia_gauntlet(self):
+        self.going_trivia = 6
+        Screen.current=1
+        Screen.switch_frame() 
+        
+    def go_highscores(self):
+        Screen.current=3
+        Screen.switch_frame()     
+        
+class Trivia(Screen):
+    
+    def __init__(self):
+        Screen.__init__(self)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1) 
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+        self.grid_rowconfigure(7, weight=1)
+        self.grid_rowconfigure(8, weight=1)
+        
+        
+        self.lbl_question_num = tk.Label(self,text="Question:", font=LABEL_FONT)
+        self.lbl_question_num.grid(row=0,column=0,sticky="news")
+        
+        self.lbl_question_num_out_of_num = tk.Label(self,text="# / #", font=LABEL_FONT)
+        self.lbl_question_num_out_of_num.grid(row=1,column=0,sticky="news")
+        
+        self.lbl_trivia_score = tk.Label(self,text="Your Score: #", font=LABEL_FONT)
+        self.lbl_trivia_score.grid(row=0,column=3,sticky="news")
+        
+        self.lbl_trivia_highscore = tk.Label(self,text="High Score: #", font=LABEL_FONT)
+        self.lbl_trivia_highscore.grid(row=1,column=3,sticky="news")
+        
+        self.lbl_trivia_highscore = tk.Label(self,text="Question", font=TITLE_FONT)
+        self.lbl_trivia_highscore.grid(row=2,column=0,rowspan=2, columnspan=4,sticky="news")
+        
+        self.frm_filters = AnswerButtons(self)
+        self.frm_filters.grid(row=4,column=0,rowspan=3,columnspan=4,sticky="news")
+        
+        '''self.lbl_trivia_highscore = tk.Label(self,text="", font=LABEL_FONT)
+        self.lbl_trivia_highscore.grid(row=,column=,sticky="news") '''       
+        
+
+        
+        
+        
+
+class Conclusion(Screen):
+    
+    def __init__(self):
+        Screen.__init__(self)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        
+        
+class Highscores(Screen):
+    
+    def __init__(self):
+        Screen.__init__(self)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
 
 
+class AnswerButtons(tk.Frame):
+    
+    def __init__(self,master):
+        tk.Frame.__init__(self,master)
+        
+        self.btn_comedy = tk.Radiobutton(self, text = "Comedy", value = "comedy.")#, variable = self.favorite, command = self.update_text)
+        self.btn_comedy.grid(row = 0, column = 0, sticky = "news")
+       
+        self.btn_drama = tk.Radiobutton(self, text = "Drama", value = "drama.")#, variable = self.favorite, command = self.update_text)
+        self.btn_drama.grid(row = 1, column = 0, sticky = "news")
+       
+        self.btn_romance = tk.Radiobutton(self, text = "Romance", value = "romance.")#, variable = self.favorite, command = self.update_text)
+        self.btn_romance.grid(row = 2, column = 0, sticky = "news")
+        
+        
 #---Global Functions---
 
 
@@ -32,10 +206,16 @@ if __name__ == "__main__":
     
     
     root = tk.Tk()
-    root.title("Media Library")
-    root.geometry("500x500")
+    root.title("Trivia Game")
+    root.geometry("480x600")
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
+    
+    screens = [MainMenu(), Trivia(), Conclusion(), Highscores()]
+    screens[0].grid(row=0,column=0,sticky="news")
+    screens[1].grid(row=0,column=0,sticky="news")
+    screens[2].grid(row=0,column=0,sticky="news")
+    screens[3].grid(row=0,column=0,sticky="news")
     
     
     screens[0].tkraise()
